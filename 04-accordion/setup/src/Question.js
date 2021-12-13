0c9d74c6bcea203a6b52d417a0e609e842eb939a
sara.kuenzi <48669509+skuenzi24@users.noreply.github.com>
2021-12-12 20:24:57 -0700

    finished project 04

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title, info}) => {
+  const [display, setDisplay] = useState(false)
+
+
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button 
+          className='btn' 
+          onClick={() => setDisplay(!display)}
+        >
+          {display ? <AiOutlineMinus/> : <AiOutlinePlus />}
+        </button>
+      </header>
+      <p>{display && info}</p>
+    </article>
+  );
 };
 
 export default Question;
